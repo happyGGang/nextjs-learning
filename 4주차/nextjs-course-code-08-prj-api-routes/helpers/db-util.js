8497d24138909df7154dbacff76ff0412793fdc9
happyGGang <bme17114508@cu.ac.kr>
2023-11-27 15:11:59 +0900

    mongodb server url 변경

--- 4주차/nextjs-course-code-08-prj-api-routes/helpers/db-util.js
+++ 4주차/nextjs-course-code-08-prj-api-routes/helpers/db-util.js
@@ -1,8 +1,8 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
   const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
+    "mongodb+srv://happyGGang:<password>@cluster0.rz6fbpn.mongodb.net/?retryWrites=true&w=majority"
   );
 
   return client;
@@ -19,11 +19,7 @@ export async function insertDocument(client, collection, document) {
 export async function getAllDocuments(client, collection, sort) {
   const db = client.db();
 
-  const documents = await db
-    .collection(collection)
-    .find()
-    .sort(sort)
-    .toArray();
+  const documents = await db.collection(collection).find().sort(sort).toArray();
 
   return documents;
 }
